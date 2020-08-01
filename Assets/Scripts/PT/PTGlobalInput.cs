af22acab0dbd958ea16de593fdc558fcc54f5a5e
James McCafferty <jamesmccafferty@live.ca>
2020-08-01 16:16:35 -0400

    Stupid bug out of the way

---
+++ Assets/Scripts/PT/PTGlobalInput.cs
@@ -0,0 +1,53 @@
+﻿using Game;
+using HexUN.Events;
+using HexUN.MonoB;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
+
+namespace PT
+{
+    /// <summary>
+    /// Protoglobal input
+    /// </summary>
+    public class PTGlobalInput : AMonoSingletonPersistent<PTGlobalInput>
+    {
+        [Header("Emissions")]
+        [SerializeField]
+        private Vector2ReliableEvent _onMoveEvent;
+
+        [Header("Input")]
+        [SerializeField]
+        private InputActionAsset _actions;
+
+        [SerializeField]
+        private string _rewindActionName = "ActionWest";
+
+        void Awake()
+        {
+            // rewind action
+            InputAction rewind = _actions.FindAction(_rewindActionName);
+
+            rewind.started += Rewind_started;
+            rewind.canceled += Rewind_canceled;
+
+            _actions.FindAction("Move").started += PTGlobalInput_started; ;
+        }
+
+        private void PTGlobalInput_started(CallbackContext obj)
+        {
+            _onMoveEvent.Invoke(obj.ReadValue<Vector2>());
+        }
+
+        // Rewind callbacks
+        private void Rewind_started(CallbackContext obj)
+        {
+            TimeManager.Instance.ChangeTimeScale(0.2f);
+
+        }
+        private void Rewind_canceled(CallbackContext obj)
+        {
+            TimeManager.Instance.ChangeTimeScale(1f);
+        }
+    }
+}
---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,60 @@
+﻿using HexCS.Mathematics;
+using HexUN.Animation;
+using HexUN.MonoB;
+using UnityEngine;
+
+namespace Game
+{
+    /// <summary>
+    /// Singleton for managing time
+    /// </summary>
+    public class TimeManager : AMonoSingletonPersistent<TimeManager>
+    {
+        private int _inteprolationId;
+
+        private float _initalFixedDeltaTime;
+        private float _currentTimeScale = 1;
+
+        public EEasingFunction EaseFunction;  
+        public float EaseDuration = 1;
+
+        public float CurrentTimeScale => _currentTimeScale;
+
+        public void ChangeTimeScale(float scale)
+        {
+            // Validate
+            scale = Mathf.Clamp(scale, float.Epsilon, float.MaxValue);
+
+            //Set up interpolation
+            IInterpolationToken t = InterpolationManager.Instance.StartInterpolation(
+                _inteprolationId,
+                EaseDuration,
+                new SInterpolation
+                (
+                    _currentTimeScale,
+                    scale,
+                    EaseFunction
+                )
+            );
+
+            // set up events
+            t.OnInterpolationSubscriber.Subscribe(HandleTimeScaleInterpolation);
+        }
+
+        protected override void MonoAwake()
+        {
+            base.MonoAwake();
+            _inteprolationId = InterpolationManager.Instance.GetUniqueId();
+            _initalFixedDeltaTime = Time.fixedDeltaTime;
+        }
+
+        private void HandleTimeScaleInterpolation(float[] newTimeScale)
+        {
+            float scale = newTimeScale[0];
+            _currentTimeScale = scale;
+
+            Time.timeScale = scale;
+            Time.fixedDeltaTime = _initalFixedDeltaTime * scale;
+        }
+    }
+}
